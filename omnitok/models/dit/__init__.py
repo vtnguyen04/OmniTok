e54c0ca38d8d8b7ac250242747d2a17dd0e8616b
vtnguyen04 <thcs2nguyen@gmail.com>
2026-05-04 00:03:33 +0700

    feat(stage2): Add LightningDiT and SiT models for Phase 9

--- omnitok/models/dit/__init__.py
+++ omnitok/models/dit/__init__.py
@@ -1,1 +1,12 @@
-"""Dit module."""
+from omnitok.registry import Registry
+
+from .lightning_dit import LightningDiT_models
+from .sit import SiT_models
+
+DIT_REGISTRY = Registry("DiT")
+
+for name, model_fn in SiT_models.items():
+    DIT_REGISTRY.register(name)(model_fn)
+
+for name, model_fn in LightningDiT_models.items():
+    DIT_REGISTRY.register(name)(model_fn)
